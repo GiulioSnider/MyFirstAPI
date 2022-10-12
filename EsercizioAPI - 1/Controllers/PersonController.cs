53dde0edac90bd005400d2fcfcdba40a9983413c
GiulioSnider <giuliosnider@gmail.com>
2022-10-12 12:40:58 +0200

    Primi passi nella creazione del controller: primi 3 metodi

---
+++ EsercizioAPI - 1/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using EsercizioAPI___1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EsercizioAPI___1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private List<Person> people;
+        private Person person;
+
+        [HttpGet()]
+        public string Ciao()
+        {
+            return "Ciao";
+        }
+
+        [HttpGet("initialize")]
+        public void InizializeList()
+        {
+            people = new List<Person>();
+        }
+
+        [HttpGet("createPerson/{id}")]
+        public void CreatePersonQueryParam(int id, string name, string surname)
+        {
+            person = new Person(id, name, surname);
+            Console.WriteLine($"Id: {person.Id} Name: {person.Name} Surname: {person.Surname}");
+        }
+    }
+}
--- EsercizioAPI - 1/Models/Person.cs
+++ EsercizioAPI - 1/Models/Person.cs
@@ -1,9 +1,18 @@
-﻿namespace EsercizioAPI___1.Models
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+
+namespace EsercizioAPI___1.Models
 {
     public class Person
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+
+        public Person(int id, string name, string surname)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+        }
     }
 }
